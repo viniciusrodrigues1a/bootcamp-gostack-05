442cda297861ecdf9757456762bc56901ccb45fc
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-02-24 16:30:42 -0300

    :sparkles: página para mostrar informações de um repositório

---
+++ src/pages/Repository/styles.js
@@ -0,0 +1,115 @@
+import styled from 'styled-components';
+import rotate from '../../animations/rotate';
+
+export const Loading = styled.div`
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  div {
+    display: flex;
+    flex-direction: column;
+
+    svg {
+      margin: 0.5em auto;
+      animation: ${rotate} 2s linear infinite;
+    }
+  }
+`;
+
+export const Owner = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  a {
+    color: #7159c1;
+    font-size: 16px;
+    text-decoration: none;
+  }
+
+  img {
+    width: 120px;
+    border-radius: 50%;
+    margin-top: 20px;
+  }
+
+  h1 {
+    font-size: 24px;
+    margin-top: 10px;
+  }
+
+  p {
+    font-size: 14px;
+    margin-top: 5px;
+    color: #666;
+    line-height: 1.4;
+    text-align: center;
+    max-width: 400px;
+  }
+`;
+
+export const IssueList = styled.ul`
+  padding-top: 30px;
+  margin-top: 30px;
+  border-top: 1px solid #eee;
+  list-style: none;
+
+  li {
+    display: flex;
+    padding: 15px 10px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+
+    & + li {
+      margin-top: 10px;
+    }
+
+    img {
+      width: 36px;
+      height: 36px;
+      border-radius: 50%;
+      border: 2px solid #eee;
+    }
+
+    div {
+      flex: 1;
+      margin-left: 15px;
+
+      span {
+        font-size: 16px;
+        font-weight: bold;
+
+        a {
+          text-decoration: none;
+          color: #333;
+
+          &:hover {
+            color: #7159c1;
+          }
+        }
+
+        span {
+          background: #7159c144;
+          color: #333;
+          border-radius: 2px;
+          font-size: 12px;
+          font-weight: 600;
+          height: 20px;
+          padding: 3px 4px;
+          margin-left: 10px;
+        }
+      }
+
+      p {
+        margin-top: 5px;
+        font-size: 12px;
+        color: #999;
+      }
+    }
+  }
+`;
