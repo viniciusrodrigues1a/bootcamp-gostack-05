56d7c25376fabcc03295a276736c7346dfd3466a
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-02-24 17:08:18 -0300

    :lipstick: adicionando nono animation

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,4 +1,14 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
+
+const nono = keyframes`
+  0%, 100% {
+    transform: translateX(0);
+  } 25% {
+    transform: translateX(-7%);
+  } 75% {
+    transform: translateX(7%);
+  }
+`;
 
 const Container = styled.div`
   max-width: 700px;
@@ -7,6 +17,12 @@ const Container = styled.div`
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
   padding: 30px;
   margin: 80px auto;
+  animation: ${props =>
+    props.error &&
+    css`
+      ${nono} 0.26s linear infinite
+    `};
+  border: ${props => (props.error ? '2px solid #f00' : 'none')};
 
   h1 {
     font-size: 20px;
