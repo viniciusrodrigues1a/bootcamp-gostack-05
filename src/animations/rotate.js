e2f72cab5482f355eac657f4154aab969958970d
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-02-24 16:29:43 -0300

    :sparkles: pasta de animations

---
+++ src/animations/rotate.js
@@ -0,0 +1,11 @@
+import { keyframes } from 'styled-components';
+
+const rotate = keyframes`
+from {
+  transform: rotate(0deg);
+} to {
+  transform: rotate(360deg);
+}
+`;
+
+export default rotate;
